a338a325a5ce8baef150de993723355c99824cdc
w4ngzhen <w4ngzhen@hotmail.com>
2023-12-12 16:10:52 +0800

    init project.

---
+++ cef_app/process_helper.mm
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  std::cout << "hello, this is sub helper process." << std::endl;
+  return 0;
+}
---
+++ cef_app/process_main.mm
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  std::cout << "hello, this is main process." << std::endl;
+  return 0;
+}
